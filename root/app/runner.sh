4f22e888638eef44c3dfcccced447de168a4fe12
Vincent Filby <vfilby@debian1.debian1>
2024-11-05 21:28:46 -0800

    Removing unnecessary venv

--- root/app/runner.sh
+++ root/app/runner.sh
@@ -3,12 +3,7 @@
 # Default to running every 60s
 INTERVAL="${ORGANIZE_INTERVAL:-60}"
 
-source /app/bin/activate
-
 echo "Starting... will organize every $INTERVAL seconds."
-echo `which python`
-echo `which organize`
-
 
 while true; do
   organize run /app/organize-rules.yaml
