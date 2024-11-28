d019b796c0f9acece6abf1901a738ddd87a1a19f
Vincent Filby <vfilby@debian1.debian1>
2024-11-27 19:20:54 -0800

    Default to sim, override with ENV

--- root/app/runner.sh
+++ root/app/runner.sh
@@ -2,10 +2,15 @@
 
 # Default to running every 60s
 INTERVAL="${ORGANIZE_INTERVAL:-60}"
+if [ "$SIMULATE" = "false" ]; then
+  CMD="run"
+else
+  CMD="sim"
+fi
 
 echo "Starting... will organize every $INTERVAL seconds."
 
 while true; do
-  organize run /app/organize-rules.yaml
+  echo organize $CMD /app/organize-rules.yaml
   sleep $INTERVAL
 done
